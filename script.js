9373e91e148c0abcc486a670e28536b792bcb77b
Soumya Ranjan <www.soumyaranjansahoo17@gmail.com>
2024-08-10 00:09:48 +0530

    added functionality to handel images

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function convert() {
     const htmlInput = document.getElementById('htmlInput').value;
     const markdownOutput = document.getElementById('markdownOutput');
-    
+
     // Basic conversion function
     function htmlToMarkdown(html) {
         let markdown = html;
-        
+
         // Convert headings
         markdown = markdown.replace(/<h1>(.*?)<\/h1>/g, '# $1');
         markdown = markdown.replace(/<h2>(.*?)<\/h2>/g, '## $1');
@@ -13,26 +13,30 @@ function convert() {
         markdown = markdown.replace(/<h4>(.*?)<\/h4>/g, '#### $1');
         markdown = markdown.replace(/<h5>(.*?)<\/h5>/g, '##### $1');
         markdown = markdown.replace(/<h6>(.*?)<\/h6>/g, '###### $1');
-        
-        // Convert paragraphs
+
+        // Convert paragraphs (preserve line breaks)
+        markdown = markdown.replace(/<\/p>\s*<p>/g, '\n\n');
         markdown = markdown.replace(/<\/?p>/g, '');
-        
+
         // Convert links
         markdown = markdown.replace(/<a href="(.*?)">(.*?)<\/a>/g, '[$2]($1)');
-        
-        // Convert images
-        markdown = markdown.replace(/<img src="(.*?)" alt="(.*?)" \/>/g, '![$2]($1)');
-        
+
+        // Convert images (including handling malformed src)
+        markdown = markdown.replace(/<img\s+src="([^"]+)"\s+alt="([^"]*)"\s*\/?>/g, '![$2]($1)');
+
         // Convert bold
         markdown = markdown.replace(/<strong>(.*?)<\/strong>/g, '**$1**');
-        
+
         // Convert italic
         markdown = markdown.replace(/<em>(.*?)<\/em>/g, '*$1*');
-        
+
         // Convert underline
         markdown = markdown.replace(/<u>(.*?)<\/u>/g, '_$1_');
-        
-        return markdown;
+
+        // Clean up remaining HTML tags (just in case)
+        markdown = markdown.replace(/<\/?[^>]+>/g, '');
+
+        return markdown.trim();
     }
 
     markdownOutput.textContent = htmlToMarkdown(htmlInput);
---
+++ testing.md
@@ -0,0 +1,19 @@
+# Main Heading
+## Subheading Level 2
+
+    
+This is a paragraph of text with two images below:
+
+    
+Here is the first image: ![First Reference Image](https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBJQeF5WofXECCFt4M-0ZAFWya_vNt1jz11Q&s)
+
+Here is the second image:
+    ![Second Reference Image](https://cdn.pixabay.com/photo/2017/08/23/15/39/square-2673252_640.png)
+
+    
+    Combine text with images:
+
+This paragraph contains an image:
+    ![First Reference Image](https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBJQeF5WofXECCFt4M-0ZAFWya_vNt1jz11Q&s)
+    And another image:
+    ![Second Reference Image](https://cdn.pixabay.com/photo/2017/08/23/15/39/square-2673252_640.png)
